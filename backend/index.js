d0f9d5993ed6f8d6f18c35349e75c5495f4e0b4b
prasanthrio99 <prasanthraja097@gmail.com>
2024-07-22 17:31:04 +0530

    database connection

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import connectDB from "./database/db.js";
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
@@ -10,4 +11,5 @@ app.get("/",(req,res)=>{
 
 app.listen(port ,() => {
     console.log("Server Run");
+    connectDB();
 });
